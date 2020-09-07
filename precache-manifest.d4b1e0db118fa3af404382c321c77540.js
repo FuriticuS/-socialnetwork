9ab67cba201548c49d68bdc9f68813a126ec3cfe
denist <Fur1ouS@ya.ru>
2020-09-07 18:44:12 +0300

    thri add files

---
+++ precache-manifest.d4b1e0db118fa3af404382c321c77540.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1800c662968a09ab7a5fba0a067f7cd7",
+    "url": "/-socialnetwork/index.html"
+  },
+  {
+    "revision": "538792f2a4c0df58d4e1",
+    "url": "/-socialnetwork/static/css/3.f59ebdb3.chunk.css"
+  },
+  {
+    "revision": "fd93a73fa026a8699e46",
+    "url": "/-socialnetwork/static/css/4.8e1f2c33.chunk.css"
+  },
+  {
+    "revision": "d2a65e2cbd3de8190a33",
+    "url": "/-socialnetwork/static/css/5.c71ad694.chunk.css"
+  },
+  {
+    "revision": "410522710e9b0be4104d",
+    "url": "/-socialnetwork/static/css/6.1177ab53.chunk.css"
+  },
+  {
+    "revision": "5be24e178d8342b62a70",
+    "url": "/-socialnetwork/static/css/7.3e66a396.chunk.css"
+  },
+  {
+    "revision": "abd977078f5defdead29",
+    "url": "/-socialnetwork/static/css/main.cc52e995.chunk.css"
+  },
+  {
+    "revision": "2fdcc5e4170281b69c91",
+    "url": "/-socialnetwork/static/js/2.a3cd5e80.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/-socialnetwork/static/js/2.a3cd5e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538792f2a4c0df58d4e1",
+    "url": "/-socialnetwork/static/js/3.36a46361.chunk.js"
+  },
+  {
+    "revision": "fd93a73fa026a8699e46",
+    "url": "/-socialnetwork/static/js/4.6ca81927.chunk.js"
+  },
+  {
+    "revision": "d2a65e2cbd3de8190a33",
+    "url": "/-socialnetwork/static/js/5.104d3594.chunk.js"
+  },
+  {
+    "revision": "410522710e9b0be4104d",
+    "url": "/-socialnetwork/static/js/6.0890141b.chunk.js"
+  },
+  {
+    "revision": "5be24e178d8342b62a70",
+    "url": "/-socialnetwork/static/js/7.b992f5fd.chunk.js"
+  },
+  {
+    "revision": "abd977078f5defdead29",
+    "url": "/-socialnetwork/static/js/main.8879c332.chunk.js"
+  },
+  {
+    "revision": "560499e7e6048ae803ad",
+    "url": "/-socialnetwork/static/js/runtime-main.846e52ef.js"
+  },
+  {
+    "revision": "f90ea62456b9ef863a45e3662f7af95a",
+    "url": "/-socialnetwork/static/media/loader.f90ea624.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.854ccd3707ea2e0ebac90fea6c1c26f6.js"
+  "/-socialnetwork/precache-manifest.d4b1e0db118fa3af404382c321c77540.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/-socialnetwork/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
